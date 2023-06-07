e1c7e5c709a786b728ffa5d12db3d14b9b834fc9
m0hamm8d <mm994225@gmail.com>
2023-06-06 18:07:01 -0700

    8 task

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    rs = ""
+    for i in str:
+        if (ord(i) >= ord('a') and ord(i) <= ord('z')):
+            rs += chr(ord(i) - 32)
+        else:
+            rs += i
+    print(rs)
