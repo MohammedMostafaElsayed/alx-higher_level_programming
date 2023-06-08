93adf9f7a8af22e012b6ee8688fbbcf3313af09d
m0hamm8d <mm994225@gmail.com>
2023-06-08 16:43:55 -0700

    2 task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     if n == 0:
         print("{:d} arguments.".format(n))
     elif n == 1:
-        print("{:d} argument:\n{:d}: {:s}".format(n, n,argv[n]))
+        print("{:d} argument:\n{:d}: {:s}".format(n, n, argv[n]))
     else:
         print("{:d} arguments:".format(n))
         for i in range(1, n + 1):
