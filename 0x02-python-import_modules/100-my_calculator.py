c7a8a6aa44c27e4d3e4ff82c2487168034f3cb33
mohammed <mm994225@gmail.com>
2023-06-14 00:16:27 +0300

    100 task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-from sys import argv, exit
-n = len(argv)
-sum = 0
-if n == 4:
-    x1 = int(argv[1])
-    x2 = int(argv[3])
-    if argv[2] == '+':
-       sum = add(x1, x2)
-    elif argv[2] == '-':
-        sum = sub(x1, x2)
-    elif argv[2] == '*':
-        sum = mul(x1, x2)
-    elif argv[2] == '/':
-        sum = div(x1, x2)
-else:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-print(sum)
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+    n = len(argv)
+    sum = 0
+    if n == 4:
+        x1 = int(argv[1])
+        x2 = int(argv[3])
+        if argv[2] == '+':
+            sum = add(x1, x2)
+        elif argv[2] == '-':
+            sum = sub(x1, x2)
+        elif argv[2] == '*':
+            sum = mul(x1, x2)
+        elif argv[2] == '/':
+            sum = div(x1, x2)
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    print(sum)
