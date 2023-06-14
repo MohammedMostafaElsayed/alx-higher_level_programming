5bf42223fd81c58207107288b219f478c3a64374
mohammed <mm994225@gmail.com>
2023-06-14 18:11:51 +0300

    1 task

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new = my_list[:]
-    for i in range(1, len(new)):
+    for i in range(0, len(new)):
         if new[i] == search:
             new[i] = replace
     return new
