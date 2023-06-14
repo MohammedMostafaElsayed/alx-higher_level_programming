9661cdd9580ddd8b597952c0195eba7d9f141ede
mohammed <mm994225@gmail.com>
2023-06-14 22:42:28 +0300

    7 task

--- 0x04-python-more_data_structures/7-main.py
+++ 0x04-python-more_data_structures/7-main.py
@@ -2,7 +2,7 @@
 u = __import__('7-update_dictionary').update_dictionary
 p = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-a = { 'language': "C", 'number': 89, 'track': "Low level" }
+a = {'language': "C", 'number': 89, 'track': "Low level"}
 new_dict = u(a, 'language', "Python")
 p(new_dict)
 print("--")
