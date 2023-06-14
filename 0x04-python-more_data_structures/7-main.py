3a8fd488e2d3a5ddb7d588da4803de26a50d45cd
mohammed <mm994225@gmail.com>
2023-06-14 22:41:04 +0300

    7 task

--- 0x04-python-more_data_structures/7-main.py
+++ 0x04-python-more_data_structures/7-main.py
@@ -2,16 +2,16 @@
 u = __import__('7-update_dictionary').update_dictionary
 p = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-new_dict = u(a_dictionary, 'language', "Python")
+a = { 'language': "C", 'number': 89, 'track': "Low level" }
+new_dict = u(a, 'language', "Python")
 p(new_dict)
 print("--")
-p(a_dictionary)
+p(a)
 
 print("--")
 print("--")
 
-new_dict = u(a_dictionary, 'city', "San Francisco")
+new_dict = u(a, 'city', "San Francisco")
 p(new_dict)
 print("--")
-p(a_dictionary)
+p(a)
