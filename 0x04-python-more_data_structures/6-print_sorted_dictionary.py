7a0c2a306cd1d22da8c36427c76ab8d5a895b5b1
mohammed <mm994225@gmail.com>
2023-06-14 21:33:04 +0300

    6 task

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    k = list(a_dictionary.keys())
-    v = list(a_dictionary.values())
+    k = list(a_dictionary.items())
+    k.sort()
+    d = dict(k)
+    k = list(d.keys())
+    v = list(d.values())
     x = map((lambda q, z: "{}: {}".format(q, z)), k, v)
     for i in x:
         print(i)
