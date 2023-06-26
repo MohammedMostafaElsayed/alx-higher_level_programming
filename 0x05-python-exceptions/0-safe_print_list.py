47244311431ec3fc26ccac378d93aa4c002e9458
mohammed <mm994225@gmail.com>
2023-06-26 12:04:38 +0300

    0 task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(0, x):
             print("{:d}".format(my_list[i]), end="")
             z = z + 1
-    except IndexError:
+    except TypeError:
         pass
-    print("")
-    return z
+    finally:
+        print("")
+        return z
