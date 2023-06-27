d26939e959ab61102be3f881ef5ceaf63fb6a51d
mohammed <mm994225@gmail.com>
2023-06-27 13:37:15 +0300

    1 task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,10 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """Define a class Square."""
 
 
 class Square:
     """Represent a square."""
-    
+
     __size = None
     def __init__(self, size):
         self.__size = size
